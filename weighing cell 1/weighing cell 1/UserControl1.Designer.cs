f70b461d0155b8bcf51f5d004b648d64d22ac693
poposoo <15099500838@163.com>
2025-04-22 22:27:37 +0800

    Add files via upload

---
+++ weighing cell 1/weighing cell 1/UserControl1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace weighing_cell_1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始采集";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(128, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(128, 73);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(127, 105);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 26);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(128, 137);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 26);
+            this.comboBox4.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "通讯端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "波 特 率";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "采样周期";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "采集地址";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(336, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(465, 36);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "PBST-YB型数据智能采集软件";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 312);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数据采集设定";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 42);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "退出系统";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 42);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "清除屏幕";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(16, 387);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 108);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "实时数据";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(44, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 39);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1182, 668);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ weighing cell 1/weighing cell 1/UserControl1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.IO; 
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+
+
+
+namespace weighing_cell_1
+{
+    public partial class UserControl1 : UserControl
+    {
+        private SerialPort _serialPort;
+        private Timer _samplingTimer;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+        // 初始化控件内容
+        private void InitializeControls()
+        {
+            // 填充COM端口
+            comboBox1.Items.AddRange(new[] { "COM1", "COM2", "COM3", "COM4", "COM5" });
+            comboBox1.SelectedIndex = 0;
+
+            // 填充波特率
+            comboBox2.Items.AddRange(new object[] { 2400, 4800, 9600, 19200 });
+            comboBox2.SelectedIndex = 2; // 默认9600
+
+            // 填充采样周期
+            comboBox3.Items.AddRange(new object[] { 0.1, 0.5, 1.0, 5.0, 10.0 });
+            comboBox3.SelectedIndex = 0; // 默认1s
+
+            // 填充采集地址
+            for (int i = 1; i <= 50; i++) comboBox4.Items.Add(i);
+            comboBox4.SelectedIndex = 0;
+
+            // 初始化定时器
+            _samplingTimer = new Timer { Interval = 1000 }; // 默认1秒
+            _samplingTimer.Tick += SamplingTimer_Tick;
+        }
+
+
+        // 点击“开始/停止采集”按钮
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (button1.Text == "开始采集")
+                {
+                    // 检查串口是否已打开
+                    if (_serialPort == null || !_serialPort.IsOpen)
+                    {
+                        // 获取可用端口时添加错误处理
+                        string[] ports;
+                        try
+                        {
+                            ports = SerialPort.GetPortNames();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"获取COM端口失败：{ex.Message}");
+                            return;
+                        }
+
+                        if (ports.Length == 0)
+                        {
+                            MessageBox.Show("未检测到可用COM端口，请检查设备连接！");
+                            return;
+                        }
+
+                        // 验证所选端口是否可用
+                        string selectedPort = comboBox1.SelectedItem.ToString();
+                        if (!ports.Contains(selectedPort))
+                        {
+                            MessageBox.Show($"选择的端口{selectedPort}不可用，请重新选择！");
+                            return;
+                        }
+
+                        try
+                        {
+                            // 配置串口
+                            _serialPort = new SerialPort(
+                                selectedPort,
+                                (int)comboBox2.SelectedItem,
+                                Parity.None,
+                                8,
+                                StopBits.One)
+                            {
+                                ReadTimeout = 500,
+                                WriteTimeout = 500  // 添加写入超时
+                            };
+
+                            _serialPort.Open();
+
+                            // 发送测试命令并验证响应
+                            byte[] testCommand = BuildCommandFrame(0x11, 0x44, 0x3F);
+                            _serialPort.DiscardInBuffer(); // 清除输入缓冲区
+                            _serialPort.Write(testCommand, 0, testCommand.Length);
+
+                            // 改进的响应检测
+                            int bytesRead = 0;
+                            DateTime startTime = DateTime.Now;
+                            while (bytesRead < 10 && (DateTime.Now - startTime).TotalMilliseconds < 1000)
+                            {
+                                if (_serialPort.BytesToRead > 0)
+                                {
+                                    bytesRead += _serialPort.BytesToRead;
+                                }
+                                System.Threading.Thread.Sleep(50);
+                            }
+
+                            if (bytesRead < 10)
+                            {
+                                throw new TimeoutException("传感器响应超时");
+                            }
+                        }
+                        catch (TimeoutException)
+                        {
+                            MessageBox.Show("传感器无响应，请检查连接和参数设置！");
+                            ResetConnection();
+                            return;
+                        }
+                    }
+
+                    // 启动采集
+                    _samplingTimer.Interval = (int)((double)comboBox3.SelectedItem * 1000);
+                    _samplingTimer.Start();
+                    button1.Text = "停止采集";
+                    textBox1.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    // 停止采集
+                    ResetConnection();
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("COM端口被其他程序占用或没有访问权限！");
+                ResetConnection();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"串口通信错误：{ex.Message}");
+                ResetConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"操作失败：{ex.Message}\n\n详细错误：{ex.ToString()}");
+                ResetConnection();
+            }
+        }
+
+        // 重置连接状态
+        private void ResetConnection()
+        {
+            _samplingTimer?.Stop();
+            _serialPort?.Close();
+            button1.Text = "开始采集";
+            textBox1.BackColor = Color.White;
+        }
+
+        // 定时采集任务
+        // 定时采集任务（带超时检测）
+        private DateTime _lastResponseTime = DateTime.MinValue;
+        private void SamplingTimer_Tick(object sender, EventArgs e)
+        {
+            if (_serialPort == null || !_serialPort.IsOpen) return;
+
+            try
+            {
+                // 发送读取命令
+                byte address = (byte)((int)comboBox4.SelectedItem + 0x10);
+                byte[] command = BuildCommandFrame(address, 0x42, 0x3F);
+                _serialPort.Write(command, 0, command.Length);
+
+                // 接收数据（带超时检测）
+                int timeout = 500; // 500ms超时
+                int elapsed = 0;
+                while (_serialPort.BytesToRead < 10 && elapsed < timeout)
+                {
+                    System.Threading.Thread.Sleep(50);
+                    elapsed += 50;
+                }
+
+                if (_serialPort.BytesToRead >= 10)
+                {
+                    byte[] buffer = new byte[10];
+                    _serialPort.Read(buffer, 0, 10);
+                    double weight = ParseWeightData(buffer);
+                    UpdateWeightDisplay(weight);
+                    _lastResponseTime = DateTime.Now;
+                    textBox1.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    // 连续3次无响应视为断开
+                    if ((DateTime.Now - _lastResponseTime).TotalSeconds > 3)
+                    {
+                        MessageBox.Show("传感器连接超时，请检查设备！");
+                        ResetConnection();
+                    }
+                    textBox1.BackColor = Color.LightCoral;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"数据读取失败：{ex.Message}");
+                ResetConnection();
+            }
+        }
+
+        // 构建命令帧（地址 + 命令 + 参数 + 校验和 + 结束符）
+        private byte[] BuildCommandFrame(byte address, byte commandCode, byte parameter)
+        {
+            byte[] frame = new byte[5];
+            frame[0] = address;
+            frame[1] = commandCode;
+            frame[2] = parameter;
+            frame[3] = CalculateChecksum(address, commandCode, parameter);
+            frame[4] = 0x0D; // 结束符
+            return frame;
+        }
+
+        // 计算校验和（低7位，若为0x0D则+1）
+        private byte CalculateChecksum(byte address, byte command, byte parameter)
+        {
+            int sum = address + command + parameter;
+            byte checksum = (byte)(sum & 0x7F);
+            return checksum == 0x0D ? (byte)(checksum + 1) : checksum;
+        }
+
+        // 解析重量数据（示例解析逻辑）
+        private double ParseWeightData(byte[] data)
+        {
+            // 1. 计算原始数值（必须保留此部分）
+            int rawValue = (data[5] - 0x30) * 65536 +
+                           (data[6] - 0x30) * 4096 +
+                           (data[7] - 0x30) * 256 +
+                           (data[8] - 0x30) * 16 +
+                           (data[9] - 0x30);
+
+            // 2. 解析符号和小数点
+            byte status = data[10];
+            bool isNegative = (status & 0x04) != 0;
+
+            // 3. 传统switch写法
+            int decimalPlaces;
+            switch (status & 0x03)
+            {
+                case 0b11:
+                    decimalPlaces = 3;
+                    break;
+                case 0b10:
+                    decimalPlaces = 2;
+                    break;
+                case 0b01:
+                    decimalPlaces = 1;
+                    break;
+                default:
+                    decimalPlaces = 0;
+                    break;
+            }
+
+            // 4. 计算最终重量
+            double weight = rawValue / Math.Pow(10, decimalPlaces);
+            return isNegative ? -weight : weight;
+        }
+
+        // 更新显示（UI线程安全）
+        private void UpdateWeightDisplay(double weight)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action(() =>
+                {
+                    textBox1.Text = $"{weight:F3} kg"; // 保留3位小数
+                }));
+            }
+            else
+            {
+                textBox1.Text = $"{weight:F3} kg";
+            }
+        }
+
+        // 点击“清空结果”按钮
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        // 点击“退出系统”按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _samplingTimer?.Stop();
+            _serialPort?.Close();
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+    }
+
+    }
